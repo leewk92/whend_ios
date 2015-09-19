489a61c236368b8ad036664beedab053c7f6704a
맥북 <mac@maegbugui-MacBook-Pro.local>
2015-09-19 21:06:16 +0900

    layout작업

---
+++ whend/NotificationViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationViewCell.swift
+//  whend
+//
+//  Created by 맥북 on 2015. 9. 19..
+//  Copyright (c) 2015년 SOODAL. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
